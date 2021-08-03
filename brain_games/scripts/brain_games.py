716016a98e8367acc1cbdaba99daf068e59c93c3
olya961 <olgakovernik@yandex.ru>
2021-08-03 19:54:18 +0300

    step 3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
-
+from brain_games.cli import welcome_user
+import prompt 
 
 def main():
     print("Welcome to the Brain Games!")
+    welcome_user()
 
 
 if __name__ == '__main__':
